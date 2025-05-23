c12b4d7174826cd7515df093b312c274b5ad143c
Túlio Marinho <tulio.smarinho@gmail.com>
2025-05-23 18:06:05 -0300

    first comit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function add_recourses(){
+    add_theme_support('custom-logo');
+}
+add_action('after_setup_theme', 'add_recourses');
+
+
+function basic_register_menu(){
+    register_nav_menus(array(
+        'menu-navigation'=>'Menu de navegação'
+    ));
+}
+add_action('init', 'basic_register_menu');
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/style.css' ?>">
+</head>
+<body>
+    <?php
+
+    the_custom_logo();
+
+    wp_nav_menu(
+        array(
+            'menu' =>'menu-navigation'
+        )
+    );
